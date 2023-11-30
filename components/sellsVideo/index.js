648f4007a3a23a73da644c3e0ca8c4ca4c1fd8f5
Amir Benassayag <amir12061968@gmail.com>
2023-11-30 21:45:15 +0200

    fixing sells video

--- components/sellsVideo/index.js
+++ components/sellsVideo/index.js
@@ -3,21 +3,31 @@ import { View, StyleSheet, Dimensions } from 'react-native';
 import { WebView } from 'react-native-webview';
 import Popup from '../popup';
 import { SELLS_VIDEOS } from '../../constants';
+import YoutubePlayer from 'react-native-youtube-iframe';
 
 const SellsVideo = ({ open, setIsOpen, index }) => {
     const video = SELLS_VIDEOS[index];
 
-    if (!video){
+
+    if (!video) {
         setIsOpen(false);
         return null;
     }
 
+    const slice = video.split('?')[1].split('&')[0].split('=')[1];
+
     return (
         <Popup visible={open} onClose={setIsOpen}>
             <View style={styles.sellsVideo}>
-                <WebView
+                {/*  <WebView
                     style={styles.iframe}
                     source={{ uri: video }}
+                /> */}
+                <YoutubePlayer
+                    height={200}
+                    width={280}
+                    play={true}
+                    videoId={slice}
                 />
             </View>
         </Popup>
